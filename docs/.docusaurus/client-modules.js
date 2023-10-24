a427134fdfed18bef1e23f5abb99ecb85e06ef41
joaocarazzato <joao.carazzato@sou.inteli.edu.br>
2023-10-24 14:59:17 -0300

    removido arquivos .gitignore

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/joao/Documents/GitHub/andevs/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/joao/Documents/GitHub/andevs/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/joao/Documents/GitHub/andevs/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/joao/Documents/GitHub/andevs/docs/src/css/custom.css'),
-];
