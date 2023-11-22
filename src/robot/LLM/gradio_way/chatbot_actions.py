71df77b7973f10364a87735acbc75bb6bdd37579
joaocarazzato <joao.carazzato@sou.inteli.edu.br>
2023-11-22 17:49:06 -0300

    added LLM chatbot examples
    Added examples with gradio and terminal with two different models and ways to do the regex implementation with the LLM.
    
    Co-Authored-By: rafaelkatalan <99191700+rafaelkatalan@users.noreply.github.com>

---
+++ src/robot/LLM/gradio_way/chatbot_actions.py
@@ -0,0 +1,5 @@
+def chatbot_print(objeto):
+    print(objeto)
+
+if __name__ == "__main__":
+    chatbot_print()
---
+++ src/robot/LLM/terminal_way/LLM-Andev/chatbot_actions.py
@@ -0,0 +1,26 @@
+dict_tool = {
+    "martelo": (4, 5),
+    "chave de fenda": (11, 39),
+    "serrote": (63, 51),
+    "parafusadeira": (8, 88),
+    "alicate": (75, 43),
+    "marreta": (22, 17),
+    "tesoura": (91, 12),
+    "nível": (34, 78),
+    "fita métrica": (50, 22),
+    "furadeira": (18, 93),
+    "chave inglesa": (67, 56),
+    "pá": (39, 2),
+    "seringa": (84, 71),
+    "trena": (12, 45),
+    "grampo": (97, 30),
+}
+
+def chatbot_print(objeto):
+    print(f"Indo pegar {objeto}")
+    pos = dict_tool[objeto]
+    print(f"Posição: {pos}")
+    return f"Indo pegar {objeto}"
+
+if __name__ == "__main__":
+    chatbot_print()
