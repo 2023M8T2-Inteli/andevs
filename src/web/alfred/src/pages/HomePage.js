fcf40fd385b56fd63dd20e95b1dee279de1ccd55
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2023-11-30 14:40:26 -0300

    update front

---
+++ src/web/alfred/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// Exemplo de HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      {/* Conteúdo da página inicial */}
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/web/alfred/src/pages/LoginPage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Form, Input, Button, Typography, notification, Image } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+import logo from '../styles/images/logo.png';
+
+const { Title } = Typography;
+
+const LoginPage = () => {
+  const onFinish = (values) => {
+    // Lógica de autenticação ou envio dos dados
+    console.log('Received values:', values);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <Image
+        width={200}
+        src={logo}
+        preview={false}
+      />
+      <Title level={2} style={{fontFamily:'Manrope'}}>Login</Title>
+      <Form
+        name="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        style={{ maxWidth: '300px', margin: 'auto' }}
+      >
+        <Form.Item
+          name="email"
+          rules={[
+            { required: true, message: 'Por favor, insira seu e-mail.' },
+            { type: 'email', message: 'Por favor, insira um e-mail válido.' },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="E-mail" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Por favor, insira sua senha.' }]}
+        >
+          <Input.Password prefix={<LockOutlined />} placeholder="Senha" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Entrar
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          Ainda não tem um cadastro? <Link to="/signup">Cadastre-se</Link>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginPage;
