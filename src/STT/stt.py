dab762b900b07290046ac1caa820f18d1ac51ef8
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2023-12-12 11:38:14 -0300

    Update TTS

--- src/STT/stt.py
+++ src/STT/stt.py
@@ -3,26 +3,32 @@ import openai
 import os
 import pyttsx3
 
-openai.api_key = "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"  # Substitua com sua chave API do OpenAI
-messages = [{"role": "system", "content": 'Your name is Alfred, you are a guidance robot for a warehouse at the Ambev brewery. Based on this, you know the location of parts and tools within the warehouse. And it is willing to help the user in the search for parts.Ferramentas Manuais:Caixa de Chaves de Fenda: (10, 5) Jogo de Alicates: (12, 6) Martelos: (11, 4) Equipamentos Eletrônicos: Multímetros: (15, 8) Osciloscópios: (14, 10) Fontes de Alimentação: (13, 7) Peças Mecânicas:Rolamentos: (5, 5)Engrenagens: 6, 7) Molas: (4, 6)Fios e Cabos:Caixas de Cabos Elétricos: (18, 9)Carretéis de Fios: (17, 8)Ferramentas de Soldagem:Máquinas de Solda: (8, 11)Eletrodos de Solda: (9, 12)Peças Eletrônicas:Resistores e Capacitores: (16, 5)Circuitos ntegrados: (16, 7)EPIs (Equipamentos de Proteção Individual):Capacete: (2, 2)Luvas de Segurança: (3, 3)Óculos de Proteção: (1, 2)'}]
+openai.api_key = "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"  # Substitute with your OpenAI API key
 
-def transcribe(audio):
+messages = [{"role": "system", "content": 'Você é um terapeuta. Responda a todas as entradas em 25 palavras ou menos.'}]
+
+
+def transcribe(audio, text):
     global messages
 
-    audio_filename_with_extension = audio + '.wav'
-    os.rename(audio, audio_filename_with_extension)
-    
-    audio_file = open(audio_filename_with_extension, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    if audio:
+        audio_filename_with_extension = audio + '.wav'
+        os.rename(audio, audio_filename_with_extension)
 
-    messages.append({"role": "user", "content": transcript["text"]})
+        audio_file = open(audio_filename_with_extension, "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        messages.append({"role": "user", "content": transcript["text"]})
+
+    messages.append({"role": "user", "content": text})
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-
     system_message = response["choices"][0]["message"]
     messages.append(system_message)
 
+    # Initialize pyttsx3 with Portuguese language
     engine = pyttsx3.init()
+    engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_PT-BR_MARIA_11.0')
+
     engine.say(system_message['content'])
     engine.runAndWait()
 
@@ -33,5 +39,6 @@ def transcribe(audio):
 
     return chat_transcript
 
-ui = gr.Interface(fn=transcribe, inputs=gr.Audio(type="filepath"), outputs="text")
+
+ui = gr.Interface(fn=transcribe, inputs=["audio", gr.Textbox(label="Texto")], outputs="text")
 ui.launch()
