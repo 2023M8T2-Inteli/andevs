f25e7024bf656eb35619cb7f285824b21b5377b9
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2023-12-16 17:27:25 -0300

    added tutorial page

--- src/web/alfred/src/pages/Tutorial.js
+++ src/web/alfred/src/pages/Tutorial.js
@@ -1,14 +1,30 @@
 // Tutorial.js
-import React, { useState } from 'react';
-import { Button, Typography } from 'antd';
-import TutorialPage from '../components/TutorialPage/TutorialPage';
+import React, { useState } from "react";
 
-const { Title, Paragraph } = Typography;
+import mic from "../images/microphone.svg";
+import text from "../images/text.svg";
+import check from "../images/check.svg";
+import wave from "../images/bg-wave.svg";
+
+import style from "./tutorial.module.css";
 
 const Tutorial = () => {
   const steps = [
-    { waveColor: '#FFD700', imageUrl: 'url_da_sua_imagem_1', text: 'Texto da etapa 1' },
-    { waveColor: '#00CED1', imageUrl: 'url_da_sua_imagem_2', text: 'Texto da etapa 2' },
+    {
+      waveColor: "#FFD700",
+      icon: mic,
+      text: "Para gravação, clique no íncone do microfone e pressione até terminar de falar.",
+    },
+    {
+      waveColor: "#00CED1",
+      icon: text,
+      text: "Para a digitalização, clique no ícone de texto e digite a peça desejada.",
+    },
+    {
+      waveColor: "#00CED1",
+      icon: check,
+      text: "Quando a informação for processada, selecione o check para a confirmação da informação indicada.",
+    },
     // Adicione mais etapas conforme necessário
   ];
 
@@ -19,15 +35,34 @@ const Tutorial = () => {
       setCurrentStep(currentStep + 1);
     } else {
       // Se for a última etapa, pode navegar para a próxima rota ou fazer o que for necessário
-      console.log('Final do tutorial');
+      console.log("Final do tutorial");
     }
   };
 
   return (
-    <div>
-      {/* Você pode adicionar um cabeçalho aqui, se necessário */}
-      <TutorialPage steps={steps} currentStep={currentStep} handleNext={handleNext} />
-    </div>
+    <>
+      <div
+        className={style.container}
+        style={{
+          backgroundImage: `url(${wave})`,
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className={style.bullets}>
+          {[0, 1, 2].map((e) => (
+            <span className={currentStep > e ? style.active : ""}></span>
+          ))}
+        </div>
+
+        <div className={style.circle}>
+          <img alt="" src={steps[currentStep].icon} />
+        </div>
+        <footer>
+          <h4>{steps[currentStep].text}</h4>
+          <button onClick={handleNext}>Próximo</button>
+        </footer>
+      </div>
+    </>
   );
 };
 
