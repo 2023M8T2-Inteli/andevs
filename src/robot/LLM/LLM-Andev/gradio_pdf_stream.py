b2bccf1f13003fd35d81c7878b75c21e3c63180f
joaocarazzato <joao.carazzato@sou.inteli.edu.br>
2023-12-18 19:12:09 -0300

    fixed regex problems and fixed point detection for...
    robot identification.

--- src/robot/LLM/LLM-Andev/gradio_pdf_stream.py
+++ src/robot/LLM/LLM-Andev/gradio_pdf_stream.py
@@ -1,50 +1,80 @@
-import openai
-import os
-from datetime import datetime
-import re
+from langchain.vectorstores import FAISS
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
 import gradio as gr
+import re
+from chatbot_actions import chatbot_print, create_pose_stamped
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
-from chatbot_actions import chatbot_print
+from langchain.document_loaders import PyPDFLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
 
-load_dotenv() 
-
-# Substitua 'sua-api-key' pela sua chave de API do OpenAI
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# create_pose_stamped()
 
 intent_dict = {
-    r"Indo pegar [ao]?\s*([\w\s]+)": chatbot_print
+    r"(((\*)|(\* ))\[(-?\d+\.\d+),\s*(-?\d+\.\d+)\])|(((\*)|(\* ))(-?\d+\.\d+),\s*(-?\d+\.\d+))": chatbot_print
 }
 
-def send_request(user_prompt, context):
-    # Combine o prompt do usuário com o contexto do arquivo de texto
-    full_prompt = f"{context}\n\nPergunta: {user_prompt}"
+def send_request(user_prompt):
+    # load the document and split it into chunks
+    load_dotenv()
 
-    # Use a API da OpenAI para obter a resposta do modelo GPT-3.5
-    response = openai.Completion.create(
-        model="text-davinci-002",  # Use o modelo GPT-3.5 da OpenAI
-        prompt=full_prompt,
-        max_tokens=150  # Ajuste conforme necessário
+    model = ChatOpenAI(model="gpt-3.5-turbo")
+
+    loader = PyPDFLoader("./data/points2.pdf")
+    pages = loader.load_and_split()
+
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=0
     )
 
-    return response.choices[0].text.strip()
+    vectorstore = FAISS.from_documents(pages, OpenAIEmbeddings())
+
+    retriever = vectorstore.as_retriever()
+
+    print(retriever)
+
+    template = """Responda a pergunta a seguir com a seguinte lista de ferramentas de contexto:
+    {context}
+
+    Caso seja pedido para você pegar uma ferramenta, responda apenas e somente com: "*[coordenada]"
+
+    Pergunta: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+
+    model = ChatOpenAI(model="gpt-3.5-turbo")
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | model
+    )
+
+    print("\nModel answer:\n")
+    last_string = ""
+    for s in chain.stream(user_prompt):
+        last_string += s.content
+    print(last_string)
+    return last_string
 
 def generate_response(prompt, chat_history):
-    # Carregue o contexto do arquivo de texto
-    with open("./objects.txt", "r", encoding="utf-8") as file:
-        context = file.read()
-
-    response = send_request(prompt, context)
+    response = send_request(prompt)
     chat_history.append((prompt, response))
     for key, function in intent_dict.items():
         pattern = re.compile(key)
         point = pattern.findall(response)
+        print(response)
         if point:
             function(point[0])
-
+    
     return "", chat_history
 
 with gr.Blocks() as demo:
-    title = "LLM Chatbot"
+    title="LLM Chatbot"
     chatbot = gr.Chatbot()
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
