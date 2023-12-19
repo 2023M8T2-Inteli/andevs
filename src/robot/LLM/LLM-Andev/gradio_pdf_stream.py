2dea528d2755ea343848c52f8161ffd54ac6f816
gabrdgs <99189378+gabInteli@users.noreply.github.com>
2023-12-19 17:32:10 -0300

    update stt

--- src/robot/LLM/LLM-Andev/gradio_pdf_stream.py
+++ src/robot/LLM/LLM-Andev/gradio_pdf_stream.py
@@ -3,12 +3,17 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 import gradio as gr
 import re
+import openai
+import os
 from chatbot_actions import chatbot_print, create_pose_stamped
 from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 from langchain.document_loaders import PyPDFLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY") 
 
 # create_pose_stamped()
 
@@ -18,11 +23,10 @@ intent_dict = {
 
 def send_request(user_prompt):
     # load the document and split it into chunks
-    load_dotenv()
 
     model = ChatOpenAI(model="gpt-3.5-turbo")
 
-    loader = PyPDFLoader("./data/points2.pdf")
+    loader = PyPDFLoader("./data/points3.pdf")
     pages = loader.load_and_split()
 
     text_splitter = CharacterTextSplitter(
@@ -61,15 +65,37 @@ def send_request(user_prompt):
     print(last_string)
     return last_string
 
-def generate_response(prompt, chat_history):
-    response = send_request(prompt)
-    chat_history.append((prompt, response))
-    for key, function in intent_dict.items():
-        pattern = re.compile(key)
-        point = pattern.findall(response)
-        print(response)
-        if point:
-            function(point[0])
+def transcribe(audio):
+    audio_filename_with_extension = audio + '.wav'
+    os.rename(audio, audio_filename_with_extension)
+
+    audio_file = open(audio_filename_with_extension, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    print(transcript['text'])
+    return transcript['text']
+
+def generate_response(prompt=None, audio_prompt=None, chat_history=None):
+    if audio_prompt:
+        audio_stt = transcribe(audio_prompt)
+        response = send_request(audio_stt)
+        chat_history.append((audio_stt, response))
+        for key, function in intent_dict.items():
+            pattern = re.compile(key)
+            point = pattern.findall(response)
+            print(response)
+            if point:
+                function(point[0])
+    
+        return "", chat_history
+    else:
+        response = send_request(prompt)
+        chat_history.append((prompt, response))
+        for key, function in intent_dict.items():
+            pattern = re.compile(key)
+            point = pattern.findall(response)
+            print(response)
+            if point:
+                function(point[0])
     
     return "", chat_history
 
@@ -77,9 +103,12 @@ with gr.Blocks() as demo:
     title="LLM Chatbot"
     chatbot = gr.Chatbot()
     msg = gr.Textbox()
+    audio_input = gr.Audio(type="filepath")
+    submit_button = gr.Button(value="Enviar Áudio")
     clear = gr.ClearButton([msg, chatbot])
+    submit_button.click(generate_response, inputs=[msg, audio_input, chatbot], outputs=[msg, chatbot])
 
-    msg.submit(generate_response, [msg, chatbot], [msg, chatbot])
+    msg.submit(generate_response, [msg, audio_input, chatbot], [msg, chatbot])
 
 # Execute a interface Gradio
 if __name__ == "__main__":
